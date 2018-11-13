9c229860ca3e2482ddaee57f4608f5ce2c6cb268
gongtong <gongtong@2dfire.com>
2018-11-13 23:50:39 +0800

    社会单元格格式

--- supply_string_py/string_xml_to_excel.py
+++ supply_string_py/string_xml_to_excel.py
@@ -10,30 +10,13 @@ from xml.etree.ElementTree import Element, SubElement, ElementTree
 from openpyxl.workbook import Workbook
 # 颜色
 from openpyxl.styles import  PatternFill
-from openpyxl.styles import Color, Fill
+from openpyxl.styles import Color, Fill,Font,colors,Border,Side,Alignment
 from openpyxl.cell import Cell
+
 import string_xml_constants as SC
-# # 每个sheet表头 
-# _excel_title_value_key='Key'
-# _excel_title_value_value='Value'
-# _excel_title_value_en='En'
-# _excel_title_value_thai='Thai'
+import xlwt
 
-
-# # sheet name
-# _supply_base_sheet_head_btn='supply_base_btns'
-# _supply_base_sheet_head_page='supply_base_pages'
-# _supply_base_sheet_head_msg='supply_base_msgs'
-
-# _base_sheet_head_btn='base_btns'
-# _base_sheet_head_page='base_pages'
-# _base_sheet_head_msg='base_msgs'
-
-# _buy_sheet_head_btn='buy_btns'
-# _buy_sheet_head_page='buy_pages'
-# _buy_sheet_head_msg='buy_msgs'
-
-
+su_string_file_path=r'/Users/hehongqing/Downloads/supply_string_2.xlsx'
 
 def getSheetWs(wb,sheet_title):
     if SC._supply_base_sheet_head_btn==sheet_title:
@@ -42,11 +25,30 @@ def getSheetWs(wb,sheet_title):
     else:
         ws = wb.create_sheet(sheet_title)
     ws.append({1: SC._excel_title_value_key, 2:SC._excel_title_value_value, 3: SC._excel_title_value_en, 4: SC._excel_title_value_thai})
+
+    currentRow=ws._current_row
+    cellHorizontal='center'
+
+    cell=ws['A'+str(currentRow)]
+    aCellColor='d3d3d3'
+    cell.alignment=Alignment(horizontal=cellHorizontal)
+    cell.fill=PatternFill(fill_type='lightHorizontal',bgColor=aCellColor,fgColor=aCellColor)
+
+    cell=ws['B'+str(currentRow)] 
+    bCellColor='fff0f5'
+    cell.alignment=Alignment(horizontal=cellHorizontal)
+    cell.fill=PatternFill(fill_type='lightHorizontal',bgColor=bCellColor,fgColor=bCellColor)
+
+    cell=ws['C'+str(currentRow)]
+    cCellColor='ffe4e1'
+    cell.alignment=Alignment(horizontal=cellHorizontal)
+    cell.fill=PatternFill(fill_type='lightHorizontal',bgColor=cCellColor,fgColor=cCellColor)
+
+    cell=ws['D'+str(currentRow)]
+    cell.alignment=Alignment(horizontal=cellHorizontal)
+    dCellColor='ffc0cb'
+    cell.fill=PatternFill(fill_type='lightHorizontal',bgColor=dCellColor,fgColor=dCellColor)
     return ws
-# def supplyBaseMsg(wb):
-#     ws = wb.create_sheet('supply_base_msgs')
-#     ws.append({1: 'Key', 2: 'Value', 3: 'En', 4: 'Thai'})
-#     return ws
 
 def initExcel(path):
     wb = Workbook()
@@ -58,7 +60,6 @@ def initExcel(path):
 def xmlParse(zhPath, enPath,ws):
     zh_tree = ET.parse(zhPath)
     en_tree = ET.parse(enPath)
-
     # 中文 xml  解析 生成对应的 key 和 value 
     for zh_elem in zh_tree.iter(tag='string'):
         zh_key = zh_elem.attrib['name']
@@ -75,68 +76,76 @@ def xmlParse(zhPath, enPath,ws):
                 break
         #  添加字符串到excel中
         ws.append([zh_key, zh_value, en_value])
+        # 如果en_value 字符为空，则认为该项没有翻译，进行颜色填充
+        if en_value=='':
+            currentRow=ws._current_row
+            cell=ws['C'+str(currentRow)]
+            enEmptyColor='dda0dd'
+            cell.fill=PatternFill(fill_type='lightGray',bgColor=enEmptyColor,fgColor=enEmptyColor)
 
-#  Base_btns_file 
-# supplybase_zh_btns_path = '/Users/hehongqing/WorkSpace/Android/GYLStringModule/SupplyBaseString/src/main/res/values/btns.xml'
-# supplybase_en_btns_path = '/Users/hehongqing/WorkSpace/Android/GYLStringModule/SupplyBaseString/src/main/res/values-en/btns.xml'
+            cell=ws['D'+str(currentRow+1)]
+            enEmptyColor='ff69b4'
+            cell.fill=PatternFill(fill_type='lightGray',bgColor=enEmptyColor,fgColor=enEmptyColor)
 
-# #  Base_btns_file 
-# supplybase_zh_pages_path='/Users/hehongqing/WorkSpace/Android/GYLStringModule/SupplyBaseString/src/main/res/values/pages.xml'
-# supplybase_en_pages_path='/Users/hehongqing/WorkSpace/Android/GYLStringModule/SupplyBaseString/src/main/res/values-en/pages.xml'
+def setCellProperty(worksheet):
+    for i in range(1, worksheet.max_row+1):
+        worksheet.row_dimensions[i].height=40
+        for col in range(ord('A'),ord('D')):
+            worksheet[chr(col)+str(i)].font=Font(size=16)
+            borderStype='thick'
+            borderColor='556b2f'
+            worksheet[chr(col)+str(i)].border=Border(left=Side(border_style=borderStype,color=borderColor),
+            right=Side(border_style=borderStype,color=borderColor),
+            top=Side(border_style=borderStype,color=borderColor),
+            bottom=Side(border_style=borderStype,color=borderColor))
 
-# #  Base_btns_file 
-# supplybase_zh_msgs_path='/Users/hehongqing/WorkSpace/Android/GYLStringModule/SupplyBaseString/src/main/res/values/msgs.xml'
-# supplybase_en_msgs_path='/Users/hehongqing/WorkSpace/Android/GYLStringModule/SupplyBaseString/src/main/res/values-en/msgs.xml'
-
-
-# #  Base_btns_file 
-# base_zh_btns_path='/Users/hehongqing/WorkSpace/Android/GYLStringModule/BaseString/src/main/res/values/btns.xml'
-# base_en_btns_path='/Users/hehongqing/WorkSpace/Android/GYLStringModule/BaseString/src/main/res/values-en/btns.xml'
-
-# #  Base_pages_file compare 
-# base_zh_pages_path='/Users/hehongqing/WorkSpace/Android/GYLStringModule/BaseString/src/main/res/values/pages.xml'
-# base_en_pages_path='/Users/hehongqing/WorkSpace/Android/GYLStringModule/BaseString/src/main/res/values-en/pages.xml'
-
-
-# #  Base_msgs_file compare 
-# base_zh_msgs_path='/Users/hehongqing/WorkSpace/Android/GYLStringModule/BaseString/src/main/res/values/msgs.xml'
-# base_en_msgs_path='/Users/hehongqing/WorkSpace/Android/GYLStringModule/BaseString/src/main/res/values-en/msgs.xml'
-
-
-# #  buy_btns_file compare 
-# buy_zh_btns_path='/Users/hehongqing/WorkSpace/Android/GYLStringModule/PurchaseBuyString/src/main/res/values/btns.xml'
-# buy_en_btns_path='/Users/hehongqing/WorkSpace/Android/GYLStringModule/PurchaseBuyString/src/main/res/values-en/btns.xml'
-
-# #  buy_pages_file compare 
-# buy_zh_pages_path='/Users/hehongqing/WorkSpace/Android/GYLStringModule/PurchaseBuyString/src/main/res/values/pages.xml'
-# buy_en_pages_path='/Users/hehongqing/WorkSpace/Android/GYLStringModule/PurchaseBuyString/src/main/res/values-en/pages.xml'
-
-
-# #  buy_msgs_file compare 
-# buy_zh_msgs_path='/Users/hehongqing/WorkSpace/Android/GYLStringModule/PurchaseBuyString/src/main/res/values/msgs.xml'
-# buy_en_msgs_path='/Users/hehongqing/WorkSpace/Android/GYLStringModule/PurchaseBuyString/src/main/res/values-en/msgs.xml'
-
-su_string_file_path=r'/Users/hehongqing/Downloads/supply_string_2.xlsx'
+    worksheet.column_dimensions['A'].width=60
+    worksheet.column_dimensions['B'].width=100
+    worksheet.column_dimensions['C'].width=120
+    worksheet.column_dimensions['D'].width=120
 
 def transformToExcel():
 
     # 初始化excel 的必要参数
     wb, fileName = initExcel(su_string_file_path)
-
+    ws=getSheetWs(wb,SC._supply_base_sheet_head_btn)
     # supplyBase
-    xmlParse(SC._supplybase_zh_btns_path,SC._supplybase_en_btns_path,getSheetWs(wb,SC._supply_base_sheet_head_btn))
-    xmlParse(SC._supplybase_zh_pages_path,SC._supplybase_en_pages_path,getSheetWs(wb,SC._supply_base_sheet_head_page))
-    xmlParse(SC._supplybase_zh_msgs_path,SC._supplybase_en_msgs_path,getSheetWs(wb,SC._supply_base_sheet_head_msg))
+    xmlParse(SC._supplybase_zh_btns_path,SC._supplybase_en_btns_path,ws)
+    setCellProperty(ws)
+
+    ws=getSheetWs(wb,SC._supply_base_sheet_head_page)
+    xmlParse(SC._supplybase_zh_pages_path,SC._supplybase_en_pages_path,ws)
+    setCellProperty(ws)
+
+    ws=getSheetWs(wb,SC._supply_base_sheet_head_msg)
+    xmlParse(SC._supplybase_zh_msgs_path,SC._supplybase_en_msgs_path,ws)
+    setCellProperty(ws)
 
     # base
-    xmlParse(SC._base_zh_btns_path,SC._base_en_btns_path,getSheetWs(wb,SC._base_sheet_head_btn))
-    xmlParse(SC._base_zh_pages_path,SC._base_en_pages_path,getSheetWs(wb,SC._base_sheet_head_page))
-    xmlParse(SC._base_zh_msgs_path,SC._base_en_msgs_path,getSheetWs(wb,SC._base_sheet_head_msg))
+    ws=getSheetWs(wb,SC._base_sheet_head_btn)
+    xmlParse(SC._base_zh_btns_path,SC._base_en_btns_path,ws)
+    setCellProperty(ws)
+
+    ws=getSheetWs(wb,SC._base_sheet_head_page)
+    xmlParse(SC._base_zh_pages_path,SC._base_en_pages_path,ws)
+    setCellProperty(ws)
+
+    ws=getSheetWs(wb,SC._base_sheet_head_msg)
+    xmlParse(SC._base_zh_msgs_path,SC._base_en_msgs_path,ws)
+    setCellProperty(ws)
 
     # Buy
-    xmlParse(SC._buy_zh_btns_path,SC._buy_en_btns_path,getSheetWs(wb,SC._buy_sheet_head_btn))
-    xmlParse(SC._buy_zh_pages_path,SC._buy_en_pages_path,getSheetWs(wb,SC._buy_sheet_head_page))
-    xmlParse(SC._buy_zh_msgs_path,SC._buy_en_msgs_path,getSheetWs(wb,SC._buy_sheet_head_msg))
+    ws=getSheetWs(wb,SC._buy_sheet_head_btn)
+    xmlParse(SC._buy_zh_btns_path,SC._buy_en_btns_path,ws)
+    setCellProperty(ws)
+
+    ws=getSheetWs(wb,SC._buy_sheet_head_page)
+    xmlParse(SC._buy_zh_pages_path,SC._buy_en_pages_path,ws)
+    setCellProperty(ws)
+
+    ws=getSheetWs(wb,SC._buy_sheet_head_msg)
+    xmlParse(SC._buy_zh_msgs_path,SC._buy_en_msgs_path,ws)
+    setCellProperty(ws)
 
     # 在每个 ws 对象添加节点后对文件做保存操作，不然不会生效
     wb.save(fileName)
