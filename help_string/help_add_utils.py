e6225d13c61d8ab03322aa7c4cabaf41e39bc0da
gongtong <gongtong@2dfire.com>
2019-06-04 10:55:08 +0800

    help  add utils update

--- help_string/help_add_utils.py
+++ help_string/help_add_utils.py
@@ -3,9 +3,11 @@ from help_string import help_xml_to_key_and_xml, help_check_to_result
 from normal_string import string_tw_zh_convert
 from base_lib import LanuageConvertUtils as LANG
 from help_string import help_xml_constants as HC
-
+from help_string import help_xml_covert_excel,help_excel_convet_xml_utils
 
 def toConvert():
+    help_xml_covert_excel.xmlConversionExcel()
+    help_excel_convet_xml_utils.toConvert()
     help_xml_to_key_and_xml.xmlParseToKeyValueXml()
     help_check_to_result.checkToResultXml()
     LANG.convert_zh_to_tw(HC._SOURCE_ZH_PATH, HC._SOURCE_TW_PATH)
